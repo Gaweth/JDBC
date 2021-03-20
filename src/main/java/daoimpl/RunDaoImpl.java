28d2ba6d07c81d08c4c94f390754db04092790a2
Kuba <mcdon1@wp.pl>
2021-03-20 16:04:02 +0100

    findByFragment AND findByMemberLimit - naprawione.

--- src/main/java/daoimpl/RunDaoImpl.java
+++ src/main/java/daoimpl/RunDaoImpl.java
@@ -4,10 +4,10 @@ import Entity.Run;
 import dao.RunDao;
 import database.jdbc.ultils.JdbcUtils;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+
 public class RunDaoImpl implements RunDao {
 
     public void save(Run run) throws SQLException {
@@ -25,7 +25,16 @@ public class RunDaoImpl implements RunDao {
     }
 
     public List<Run> findAll() throws SQLException {
-        return null;
+        Statement statement = JdbcUtils.getInstance().getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM runs");
+        List<Run> runList = new ArrayList<>();
+        while (resultSet.next()) {
+            runList.add(new Run(resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("members_limit")));
+        }
+        statement.close();
+        return runList;
     }
 
     public Run findById(int id) throws SQLException {
@@ -49,48 +58,79 @@ public class RunDaoImpl implements RunDao {
     }
 
     public void update(Run run) throws SQLException {
-
         PreparedStatement statement = JdbcUtils
                 .getInstance()
                 .getConnection()
                 .prepareStatement("UPDATE runs SET name=?, members_limit=?, id=?");
         statement.setString(1, run.getName());
         statement.setInt(2, run.getMembersLimit());
-        statement.setInt(3, run.getId());
+        statement.setInt(3,run.getId());
 
         statement.executeUpdate();
         statement.close();
-
-
     }
 
     public void deleteById(int id) throws SQLException {
-
-//            PreparedStatement statement = JdbcUtils
-//                    .getInstance()
-//                    .getConnection()
-//                    .prepareStatement("DELETE FROM runs WHERE id=?");
-//
-//            statement.setInt(1, id);
-//            statement.executeUpdate();
-//            statement.close();
-
         PreparedStatement statement = JdbcUtils
-               .getInstance()
-               .getConnection()
-               .prepareStatement("DELETE FROM runs WHERE id=?");
+                .getInstance()
+                .getConnection()
+                .prepareStatement("DELETE FROM runs WHERE id = ? ");
         statement.setInt(1, id);
         statement.executeUpdate();
-       statement.close();
-}
-
-
-    public List<Run> findByNameFragment(String fragment) throws SQLException {
-        return null;
+        statement.close();
     }
 
+
+
+
+  public List<Run> findByNameFragment(String fragment) throws SQLException {
+//        PreparedStatement statement = JdbcUtils
+//                .getInstance()
+//                .getConnection()
+//                .prepareStatement("SELECT * FROM runs WHERE name LIKE (%?%)");
+//        ResultSet resultSet = statement.executeQuery("SELECT * FROM runs WHERE name=%?%");
+//
+//        while (resultSet.next()) {
+//            System.out.printf("name=%?",
+//                    resultSet.getString("name"));
+//            statement.close();
+//
+//        }
+//       return null;
+
+      PreparedStatement statement = JdbcUtils
+              .getInstance()
+              .getConnection()
+              .prepareStatement("SELECT * FROM runs WHERE name LIKE CONCAT('%',?,'%')");
+      statement.setString(1, fragment);
+      ResultSet resultSet = statement.executeQuery();
+      List<Run> runList = new ArrayList<>();
+      while (resultSet.next()) {
+          runList.add(new Run(resultSet.getInt("id"),
+                  resultSet.getString("name"),
+                  resultSet.getInt("members_limit")));
+      }
+      statement.close();
+      return runList;
+  }
+
+
     public List<Run> findByMemberLimitRange(int min, int max) throws SQLException {
-        return null;
+        PreparedStatement statement = JdbcUtils
+                .getInstance()
+                .getConnection()
+                .prepareStatement("SELECT * FROM runs WHERE members_limit BETWEEN ? AND ?");
+        statement.setInt(1, min);
+        statement.setInt(2, max);
+        ResultSet resultSet = statement.executeQuery();
+        List<Run> runList = new ArrayList<>();
+        while (resultSet.next()) {
+            runList.add(new Run(resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("members_limit")));
+        }
+        statement.close();
+        return runList;
     }
 
 
